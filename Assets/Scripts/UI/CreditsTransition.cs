3c6a3b60cfef9e0a64ea814ff59682190567f238
Jonathan Menechella <jonathan.menechella@gmail.com>
2017-04-03 17:30:27 -0400

    Added Credit transition on main menu
    Credits now transition sideways to show credits instead of moving to new
    scene

---
+++ Assets/Scripts/UI/CreditsTransition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsTransition : MonoBehaviour {
+	
+	public Animator Slide;
+
+	public void CreditsPressed(){
+		Slide.SetTrigger ("SlideIn");
+	}
+
+	public void MenuPressed(){
+		Slide.SetTrigger ("SlideOut");
+	}
+}
