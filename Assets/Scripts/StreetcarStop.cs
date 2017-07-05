b3ab7e725db6f0ea089d7d0de3737110b51fe736
Adam <adam.s.vandyke@gmail.com>
2017-07-05 16:04:01 -0400

    -Refactorting bus stop script -Indicator for number of pedestrians at a stop now updates immediatly after pickups.

--- Assets/Scripts/StreetcarStop.cs
+++ Assets/Scripts/StreetcarStop.cs
@@ -19,142 +19,129 @@ public class StreetcarStop : MonoBehaviour {
 	bool streetcarStopped = false;
 
 
-	void Awake () {
-
+	void Awake ()
+    {
 		streetcarTimerCanvas = this.transform.GetChild(0).gameObject;
 		streetcarTimerCanvas.SetActive(false);
 		timerFill = streetcarTimerCanvas.GetComponentInChildren<Image>();
 	}
-
-	void Update () {
-
-		CheckForStreetcar();
+	void Update ()
+    {
+        UpdateMinimap();
+        CheckForStreetcar();
 		CheckIfFull();
 	}
-
-	void CheckForStreetcar () {
-		
-		if(streetcarStopped) {
-            
-            for (int i = 1; i < this.transform.childCount; i++) {
-				
+	void CheckForStreetcar ()
+    {
+		if(streetcarStopped)
+        {  
+            for (int i = 1; i < this.transform.childCount; i++)
+            {
 				Vector3 newDestination = this.transform.GetChild(i).position + Mathf.Sign(this.transform.position.y) * 2 * Vector3.down;
 				this.transform.GetChild(i).GetComponent<Pedestrian>().SetDestination(newDestination);
 				this.transform.GetChild(i).GetComponent<Pedestrian>().SetMoveSpeed(1.5f);
 			}
 		}
 	}
-
-	void CheckIfFull () {
-
-		if(this.transform.childCount > 5) {
-
+	void CheckIfFull ()
+    {
+		if(this.transform.childCount > 5)
+        {
 			streetcarTimerCanvas.SetActive(true);
-
-			if(streetcarTimerCanvas.activeSelf) {
-
+			if(streetcarTimerCanvas.activeSelf)
+            {
 				timerFill.fillAmount -= 0.1f * Time.deltaTime;
-
-				if(timerFill.fillAmount <= 0) {
-
-					for(int i = 1; i < this.transform.childCount; i++) {
+				if(timerFill.fillAmount <= 0)
+                {
+					for(int i = 1; i < this.transform.childCount; i++) 
+                    {
 						Destroy(this.transform.GetChild(i).gameObject, 0.5f);
 					}		
 					StartCoroutine(DelayedUpdateMinimap(0.5f));
 				}
 			}
 		}
-		else if(this.transform.childCount < 5) {
-
+		else if(this.transform.childCount < 5)
+        {
 			timerFill.fillAmount = 1f;
 			streetcarTimerCanvas.SetActive(false);
 		}
 	}
-
-	void OnTriggerStay2D(Collider2D other) {
-
-		if(other.transform.name == "Streetcar" && other.transform.GetComponent<Streetcar>() && other.transform.GetComponent<Streetcar>().IsFull() == false) {
-
+	void OnTriggerStay2D(Collider2D other)
+    {
+		if(other.transform.name == "Streetcar" && other.transform.GetComponent<Streetcar>() && other.transform.GetComponent<Streetcar>().IsFull() == false)
+        {
 			float moveSpeed = other.GetComponent<Streetcar>().GetMoveSpeed();
-
-			if(Mathf.Abs(moveSpeed) < 0.01f && !streetcarStopped) {
-
+			if(Mathf.Abs(moveSpeed) < 0.01f && !streetcarStopped)
+            {
 				streetcarStopped = true;
                 timerFill.fillAmount = 1f;
                 streetcarTimerCanvas.SetActive(false);
             }
 		}
 	}
-
-    void OnTriggerExit2D(Collider2D other) {
-
-		if(other.transform.CompareTag("Streetcar") && other.transform.GetComponent<Streetcar>() && other.transform.GetComponent<Streetcar>().IsFull() == false) {
-
+    void OnTriggerExit2D(Collider2D other)
+    {
+		if(other.transform.CompareTag("Streetcar") && other.transform.GetComponent<Streetcar>() && other.transform.GetComponent<Streetcar>().IsFull() == false)
+        {
 			streetcarStopped = false;
 			UpdateMinimap();
 		}
 	}
-
-	public bool StreetcarStopped () {
-
+	public bool StreetcarStopped ()
+    {
 		return streetcarStopped;
 	}
-
-	public void UpdateMinimap () {
-
+	public void UpdateMinimap ()
+    {
 		int pedestriansWaiting = this.transform.childCount - 1;
-
-		if(pedestriansWaiting >= 5) {
-
+		if(pedestriansWaiting >= 5)
+        {
 			minimapIconAnimator.SetTrigger("Red");
 			UpdatePedestrianAnimationSpeed(2);
 			streetcarStopSpriteRenderer.sprite = redStreetcarStop;
 		}
-		else if (pedestriansWaiting >= 3) {
-
+		else if (pedestriansWaiting >= 3)
+        {
 			minimapIconAnimator.SetTrigger("Yellow");
 			UpdatePedestrianAnimationSpeed(1.5f);
 			streetcarStopSpriteRenderer.sprite = yellowStreetcarStop;
 		}
-		else if(pedestriansWaiting >= 1) {
-
+		else if(pedestriansWaiting >= 1)
+        {
 			minimapIconAnimator.SetTrigger("Green");
 			UpdatePedestrianAnimationSpeed(1);
 			streetcarStopSpriteRenderer.sprite = greenStreetcarStop;
 		}
-		else if(pedestriansWaiting == 0) {
-
+		else if(pedestriansWaiting == 0)
+        {
 			minimapIconAnimator.SetTrigger("White");
 			UpdatePedestrianAnimationSpeed(1);
 			streetcarStopSpriteRenderer.sprite = whiteStreetcarStop;
 		}
 	}
-
-	void UpdatePedestrianAnimationSpeed (float newSpeed) {
-
-		Pedestrian[] pedestrians = this.GetComponentsInChildren<Pedestrian>();
-		foreach (Pedestrian pedestrian in pedestrians) { pedestrian.GetComponent<Animator>().speed = newSpeed; }
-	}
-
-	IEnumerator DelayedUpdateMinimap (float delayTime) {
-
+    void UpdatePedestrianAnimationSpeed(float newSpeed)
+    {
+        Pedestrian[] pedestrians = this.GetComponentsInChildren<Pedestrian>();
+        foreach (Pedestrian pedestrian in pedestrians) { pedestrian.GetComponent<Animator>().speed = newSpeed; }
+    }
+	IEnumerator DelayedUpdateMinimap (float delayTime)
+    {
 		yield return new WaitForSeconds(delayTime);
 		UpdateMinimap();
 	}
-
-	public bool HasRole(Role role) {
-
-		for(int i = 0; i < this.transform.childCount; i++) {
-
-			if(this.transform.GetChild(i).GetComponent<Pedestrian>()) {
-
-				if(this.transform.GetChild(i).GetComponent<Pedestrian>().GetRole() == role) {
-
+	public bool HasRole(Role role)
+    {
+		for(int i = 0; i < this.transform.childCount; i++)
+        {
+			if(this.transform.GetChild(i).GetComponent<Pedestrian>())
+            {
+				if(this.transform.GetChild(i).GetComponent<Pedestrian>().GetRole() == role)
+                { 
 					return true;
 				}
 			}
 		}
-
 		return false;
 	}
 }
