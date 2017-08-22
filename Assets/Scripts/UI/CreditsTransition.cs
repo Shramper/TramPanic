baabec1582aace8b51a3a8a048563d24c3a31100
SOD-SJH-607-01W\seanb_000 <stickgag@gmail.com>
2017-08-22 17:07:59 -0400

    Main Menu Rehaul - Credits move from the left, level select from the right after clicking PLAY - Small placeholder sprites for level select and some animation because it looks dead - Changed button activation from event triggers to button (script) On Click events. - Small additions in the credit transition script - Small bug in play button, won't swap sprites or animate when interacting.

--- Assets/Scripts/UI/CreditsTransition.cs
+++ Assets/Scripts/UI/CreditsTransition.cs
@@ -6,11 +6,22 @@ public class CreditsTransition : MonoBehaviour {
 	
 	public Animator Slide;
 
+	//CREDITS SLIDE
+
 	public void CreditsPressed(){
 		Slide.SetTrigger ("SlideIn");
 	}
 
 	public void MenuPressed(){
 		Slide.SetTrigger ("SlideOut");
+	}
+
+	//LEVEL SELECT SLIDE
+
+	public void LevelSelectPressed(){
+		Slide.SetTrigger ("SlideIn_LS");
+	}
+	public void LSMenuPressed(){
+		Slide.SetTrigger ("SlideOut_LS");
 	}
 }
